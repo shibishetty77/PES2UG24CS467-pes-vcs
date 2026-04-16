4e631586a33d70f39dd22ad5ba5fd02a26fcbb0d
Shibi R Shetty <shibishetty2@gmail.com>
2026-04-17 00:16:05 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -167,6 +167,7 @@ int head_update(const ObjectID *new_commit) {
     
     f = fopen(tmp_path, "w");
     if (!f) return -1;
+ 
     
     char hex[HASH_HEX_SIZE + 1];
     hash_to_hex(new_commit, hex);
